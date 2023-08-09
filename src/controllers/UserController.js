6bca2a6bfc602cd4fcd2b691586866162d3d4500
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-08-09 15:53:18 -0300

    criando funcoes para pegar informacoes do usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,7 @@
 const State = require('../models/State');
-const Info = require('../models/User');
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
 
 module.exports = {
     getStates: async (req, res) => {
@@ -8,6 +10,35 @@ module.exports = {
     },
     info: async (req, res) => {
         
+        let token = req.query.token;
+
+        const user = await User.findOne({token});
+        const state = await State.findById(user.state);
+        const ads = await Ad.find({idUser: user._id.toString()});
+        let adList = [];
+
+        for(let i in ads) {
+            const cat = await Category.findById(ads[i].category);
+            adList.push({
+                id: ads[i]._id,
+                status: ads[i].status,
+                images: ads[i].images,
+                dateCreated: ads[i].dateCreated,
+                title: ads[i].title,
+                price: ads[i].price,
+                priceNegotiable: ads[i].priceNegotiable,
+                description: ads[i].description,
+                views: ads[i].views,
+                category: cat.slug
+            });
+        }
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            state: state.name,
+            ads: adList
+        });
     },
     editAction: async (req, res) => {
         
